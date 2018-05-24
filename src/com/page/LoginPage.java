b142a6fd73af4d897520bd67d6a023fde3e7687e
Thulasi <thulasirajuad159@gmail.com>
2018-05-24 12:28:25 +0530

    Removed unwanted imports

--- src/com/page/LoginPage.java
+++ src/com/page/LoginPage.java
@@ -3,7 +3,6 @@ package com.page;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
